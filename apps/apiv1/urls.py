49502cc349e511b0c8fc17c6513ca2cd0575d86f
yejue <yejue@yjstudy1.com>
2021-07-08 18:42:41 +0800

    create: 添加一个用于便捷测试的接口

--- apps/apiv1/urls.py
+++ apps/apiv1/urls.py
@@ -1,5 +1,8 @@
+from django.urls import path
+
 from apps.configurator import views as conf_views
 from libs.routers import SimpleRouter
+from . import views as api_views
 
 app_name = "apiv1"
 
@@ -8,6 +11,8 @@ simple_router = SimpleRouter()
 simple_router.register(conf_views.ConfiguratorViewSet)
 simple_router.register(conf_views.AlgorithmViewSet)
 
-urlpatterns = []
+urlpatterns = [
+    path("visit-tester/", api_views.VisitTestView.as_view(), name="visit-tester"),
+]
 
 urlpatterns += simple_router.get_urls()
--- apps/apiv1/views.py
+++ apps/apiv1/views.py
@@ -1,3 +1,41 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import View
 
+from libs.printer import s_print
 # Create your views here.
+
+
+class VisitTestView(View):
+    """
+    请求测试接口
+     - 用于研究请求的出入流程
+     - 用于研究请求体的数据格式
+    """
+
+    def get(self, request):
+        html = []
+
+        # 1 整体请求的打印
+        # 分别打印请求对象、请求对象类型、请求对象的 __dir__
+        html.append(s_print("整体部分"))
+
+        print(request)
+        html.append(str(request))
+
+        print(type(request))
+        html.append(str(type(request)))
+
+        print(dir(request))
+        html.append(str(dir(request)))
+
+        # 2 请求中 META 部分进行打印
+        html.append(s_print("META 部分"))
+
+        print(request.META)
+        html.append(str(request.META))
+
+        # 可视化拼接
+        html = "<br>".join(html)
+
+        return HttpResponse(html)
